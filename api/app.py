6b7eb8cb0a4a4d71961a9d5427b1e512e4e95a56
Igor Nesterenya <nesterione@gmail.com>
2017-03-11 19:09:25 +0300

    Updated notes

--- api/app.py
+++ api/app.py
@@ -1,10 +1,11 @@
 #!flenv/bin/python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, jsonify 
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+
 
 @app.route('/')
 def index():
@@ -12,27 +13,44 @@ def index():
 
 
 events = [
-  {
-    'id':1,
-    'title': 'Quiz',
-    'description': 'Как обычно квиз',
-    'date': '06.03.2017 19:00',
-    'location': 'Red pub',
-    'tags': ['bg']
-  },
-  {
-    'id':2,
-    'title': 'Лазертег',
-    'description': 'Пиу-пиу. биби-биби.',
-    'date': '~19.03.2017 10:00',
-    'location': "",
-    'tags': ['sport']
-  }
+    {
+        'id': 1,
+        'title': 'Quiz',
+        'description': 'Как обычно квиз',
+        'date': '06.03.2017 19:00',
+        'location': 'Red pub',
+        'attendees': [
+            {
+                'id': 1,
+                'name': 'Igor Nesterenya',
+                'notes': 'За понедельник.'
+            },
+            {
+                'id': 2,
+                'name': 'One More',
+                'notes': '-'
+            }
+        ],
+        'created': 'date creation',
+        'tags': ['bg']
+    },
+    {
+        'id': 2,
+        'title': 'Лазертег',
+        'description': 'Пиу-пиу. биби-биби.',
+        'date': '~19.03.2017 10:00',
+        'location': "",
+        'attendees': [],
+        'created': 'date creation',
+        'tags': ['sport']
+    }
 ]
+
 
 @app.route('/api/v0.1/events', methods=['GET'])
 def get_events():
     return jsonify(events)
-     
+
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=6910)
